d534a89999e8c15a4b4af4e6f35b955b686c0373
Rahul Mistry <22cs042@charusat.edu.in>
2024-07-09 22:57:26 +0530

    History Routes and Controller done

--- server/src/controllers/history.controller.ts
+++ server/src/controllers/history.controller.ts
@@ -1,3 +1,4 @@
+import { paginationQuery } from "@/@types/misc.types";
 import History, { historyType } from "@/models/history.model";
 import { RequestHandler } from "express";
 
@@ -49,7 +50,7 @@ export const updateHistory: RequestHandler = async (req, res) => {
   ]);
 
   // const sameDayHistory = histories.find((item) => {
-  //   if (item.audio.toString() === audio) return item;
+  //   if (item.music.toString() === music) return item;
   // });
   const sameDayHistory = histories.find(
     ({ musicId }) => musicId.toString() === music
@@ -76,4 +77,154 @@ export const updateHistory: RequestHandler = async (req, res) => {
   }
 
   res.json({ success: true });
+};
+
+export const removeHistory: RequestHandler = async (req, res) => {
+  const removeAll = req.query.all === "yes";
+
+  if (removeAll) {
+    // remove all the history
+    await History.findOneAndDelete({ user: req.user.id });
+    return res.json({ success: true });
+  }
+
+  const histories = req.query.histories as string;
+  const ids = JSON.parse(histories) as string[];
+  await History.findOneAndUpdate(
+    { user: req.user.id },
+    {
+      $pull: { all: { _id: ids } },
+    }
+  );
+
+  res.json({ success: true });
+};
+
+export const getHistories: RequestHandler = async (req, res) => {
+  const { limit = "10", skip = "0" } = req.query as paginationQuery;
+  const histories = await History.aggregate([
+    { $match: { user: req.user.id } },
+    {
+      $project: {
+        all: {
+          $slice: ["$all", parseInt(limit) * parseInt(skip), parseInt(limit)],
+        },
+      },
+    },
+    { $unwind: "$all" },
+    {
+      $lookup: {
+        from: "musics",
+        localField: "all.music",
+        foreignField: "_id",
+        as: "musicInfo",
+      },
+    },
+    { $unwind: "$musicInfo" },
+    {
+      $project: {
+        _id: 0,
+        id: "$all._id",
+        musicId: "$musicInfo._id",
+        date: "$all.date",
+        title: "$musicInfo.title",
+      },
+    },
+    {
+      $group: {
+        _id: {
+          $dateToString: { format: "%Y-%m-%d", date: "$date" },
+        },
+        musics: { $push: "$$ROOT" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        id: "$id",
+        date: "$_id",
+        musics: "$$ROOT.musics",
+      },
+    },
+    { $sort: { date: -1 } },
+  ]);
+
+  res.json({ histories });
+};
+
+export const getRecentlyPlayed: RequestHandler = async (req, res) => {
+  const match = { $match: { user: req.user.id } };
+  const sliceMatch = {
+    $project: {
+      myHistory: { $slice: ["$all", 10] },
+    },
+  };
+
+  const dateSort = {
+    $project: {
+      histories: {
+        $sortArray: {
+          input: "$myHistory",
+          sortBy: { date: -1 },
+        },
+      },
+    },
+  };
+
+  const unwindWithIndex = {
+    $unwind: { path: "$histories", includeArrayIndex: "index" },
+  };
+
+  const musicLookup = {
+    $lookup: {
+      from: "musics",
+      localField: "histories.music",
+      foreignField: "_id",
+      as: "musicInfo",
+    },
+  };
+
+  const unwindmusicInfo = {
+    $unwind: "$musicInfo",
+  };
+
+  const userLookup = {
+    $lookup: {
+      from: "users",
+      localField: "musicInfo.user",
+      foreignField: "_id",
+      as: "user",
+    },
+  };
+
+  const unwindUser = { $unwind: "$user" };
+
+  const projectResult = {
+    $project: {
+      _id: 0,
+      id: "$musicInfo._id",
+      title: "$musicInfo.title",
+      about: "$musicInfo.about",
+      file: "$musicInfo.file.url",
+      thumbnail: "$musicInfo.thumbnail.url",
+      category: "$musicInfo.category.url",
+      user: { name: "$user.name", id: "$user._id" },
+      date: "$histories.date",
+      progress: "$histories.progress",
+    },
+  };
+
+  const musics = await History.aggregate([
+    match,
+    sliceMatch,
+    dateSort,
+    unwindWithIndex,
+    musicLookup,
+    unwindmusicInfo,
+    userLookup,
+    unwindUser,
+    projectResult,
+  ]);
+
+  res.json({ musics });
 };
--- server/src/routers/history.router.ts
+++ server/src/routers/history.router.ts
@@ -1,4 +1,9 @@
-import { updateHistory } from "@/controllers/history.controller";
+import {
+  getHistories,
+  getRecentlyPlayed,
+  removeHistory,
+  updateHistory,
+} from "@/controllers/history.controller";
 import { isAuth } from "@/middlewares/auth.middleware";
 import { validate } from "@/middlewares/validator";
 import { HistorySchema } from "@/utils/validation";
@@ -7,5 +12,8 @@ import { Router } from "express";
 const historyRouter = Router();
 
 historyRouter.post("/", isAuth, validate(HistorySchema), updateHistory);
+historyRouter.delete("/", isAuth, removeHistory);
+historyRouter.get("/", isAuth, getHistories);
+historyRouter.get("/recently-played", isAuth, getRecentlyPlayed);
 
 export default historyRouter;
